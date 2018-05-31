b50ba66fa54265c6c5c59bcda332b011d98051f9
ShoheiShibata <sho02130abc@gmail.com>
2018-05-31 13:30:53 +0900

    問4まで作成

---
+++ Q3.sql
@@ -0,0 +1,4 @@
+ CREATE TABLE item_category(category_id int PRIMARY KEY AUTO_INCREMENT,category_name varchar(256)NOT NULL);
+ INSERT INTO item_category VALUES(1,'â∆ãÔ');
+ INSERT INTO item_category VALUES(2,'êHïi');
+ INSERT INTO item_category VALUES(3,'ñ{');
