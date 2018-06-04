c31f54890c35c4f348801132dc74c71f7c9a796b
ShoheiShibata <sho02130abc@gmail.com>
2018-06-04 15:53:15 +0900

    問9まで作成

---
+++ Q9.sql
@@ -0,0 +1,2 @@
+ SELECT item_category.category_name,SUM(item.item_price) AS total_price FROM item_category INNER JOIN item ON item_category.category_id = item.category_id
+  GROUP BY item_category.category_name ORDER BY item.item_price DESC;
